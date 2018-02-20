f91fededdc2e60baffc00673c03e1a9088562fde
blindbycolors <mnyang1991@gmail.com>
2018-02-20 14:15:38 -0600

    update for protrait & landscape

--- app/src/main/java/com/example/yangm89/grocerytracker/MainActivity.java
+++ app/src/main/java/com/example/yangm89/grocerytracker/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.yangm89.grocerytracker;
 
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        if (getResources().getConfiguration().orientation ==
+                Configuration.ORIENTATION_LANDSCAPE) {
+            setContentView(R.layout.activity_landscape);
+        } else {
+            setContentView(R.layout.activity_portrait);
+        }
+        //this leaves the keyboard hidden on load
+        getWindow().setSoftInputMode(
+                WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
     }
 }
